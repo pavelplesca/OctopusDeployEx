77f2a41a7edf5b3ead7de0fa32bd5b0e9930aada
Plesca Pavel <plescapavel@yahoo.com>
2018-05-04 20:33:56 +0300

    Add project files.

---
+++ DbDeploy/Deploy.ps1
@@ -0,0 +1,1 @@
+﻿.\DbDeploy.exe
---
+++ DbDeploy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DbUp;
+
+namespace DbDeploy
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Server=.; Database=DbUp; User ID=DbUp;Password=123";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .LogScriptOutput()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+
+            return 0;
+        }
+    }
+}
